1390422860daa530e24bc415d3edc6bfe4b34541
Andrey Marakulin <marakulin.ap18@physics.msu.ru>
2021-02-20 21:22:17 +0300

    ads

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 # Vk-api-shell
 # Marakulin Andrey @annndruha
 # 2021
-import os
+# import os
+#
+# from shell import shell
+# # from src.followers_monitor import FollowersMonitor
+# from src.example import my_function
+#
+# if __name__ == '__main__':
+#     # FM = FollowersMonitor()
+#     target = my_function
+#     config_path = os.path.join("secret", "config.json")
+#     auth_type = "user"
+#     one_run = True
+#
+#     shell = shell.Shell(config_path=config_path, target=target, auth_type=auth_type, one_run=one_run)
+#     shell.start()
+#     shell.join()
 
-from shell import shell
-from src.followers_monitor import FollowersMonitor
 
-if __name__ == '__main__':
-    FM = FollowersMonitor()
-    target = FM.follow
-    config_path = os.path.join("secret", "config.json")
-    auth_type = "user"
-    one_run = False
+import cv2
+import glob
+import numpy as np
 
-    shell = shell.Shell(config_path=config_path, target=target, auth_type=auth_type, one_run=one_run)
-    shell.start()
-    shell.join()
+files = glob.glob("photos//*.jpg")
+for file in files:
+    name = file.split("\\")[1]
+    img = cv2.imread(file)
+    # kernel = np.ones((6,6),np.float32)/25
+    dst = cv2.GaussianBlur(img,(35,35),0)
+
+    center_coordinates = (50, 50)
+    radius = 75
+    color = (255, 255, 255)
+    thickness = 50
+    dst = cv2.circle(dst, center_coordinates, radius, color, thickness)
+
+
+    cv2.imwrite(f"photos_blur//{name}", dst)
+
--- src/example.py
+++ src/example.py
@@ -1,15 +1,21 @@
 from shell.shell import timestamp
+import requests
 
 
 def my_function(shell):
-    print(f"{timestamp()} Congratulations, it's working!\n")
+    resp = shell.vk.method('friends.get')
+    friends = resp["items"]
+    user_ids = ",".join(map(str, friends))
+    res = shell.vk.method('users.get', {'user_ids': user_ids, 'fields': 'photo_100'})
 
-    print("This is example target function or payload of your project.")
-    print("An argument is passed to the my_function is class instance of Shell")
-    print("It consist vk class object: ")
-    print(shell.vk)
-    print("for use vk methods like this:\n")
-
-    print("shell.vk.method('users.get', {'user_ids': user_ids, 'fields':'counters'})\n")
-
-    print("@annndruha 2021")
+    for r in res:
+        id = r["id"]
+        photo_100 = r["photo_100"]
+        with open(f'photos//{id}.jpg', 'wb') as handle:
+            response = requests.get(photo_100, stream=True)
+            if not response.ok:
+                print(response)
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
